34aa43a77c4622d1702b81d09b82d930d92d8701
JGusM <jgusmei@gmail.com>
2022-01-18 21:42:00 -0300

    primer commit mercadoliebre01-2022

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const app = express();
+
+const path = require("path");
+const publicPath = path.resolve(__dirname, "./public");
+
+app.use( express.static('public'))
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/home.html')
+})
+
+app.get('/register', (req, res) => {
+    res.sendFile(__dirname + '/views/register.html')
+})
+
+app.get('/login', (req, res) => {
+    res.sendFile(__dirname + '/views/login.html')
+})
+
+app.listen(porcess.env.PORT || 3000, () => {
+    console.log('servidor corriendo en puerto 3000');
+})
